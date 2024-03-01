7a419f8efbb7e9b96167424546d168af46edfcca
Pedrolucasrd <pedrolucas13rd@gmail.com>
2024-03-01 17:06:22 -0300

    Primeiro commit

---
+++ Estrategy/src/frete/FreteNormal.java
@@ -0,0 +1,8 @@
+package frete;
+
+public class FreteNormal implements FreteStrategy {
+    @Override
+    public double calcularFrete(double peso) {
+        return peso * 2.5;
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+![](https://github.com/Pedrolucasrd/Bertoti/blob/d9d61e405b4b9c97c01a55496e0cb6922dc0b28d/padroesProjetos/imagens/Strategy.png)
